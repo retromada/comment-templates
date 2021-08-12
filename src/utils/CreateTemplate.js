123cef2376fc2564ad1f1a915a0c709b1aac6a94
ruj <214782@bk.ru>
2021-08-11 22:15:11 -0300

    Line break not required

--- src/utils/CreateTemplate.js
+++ src/utils/CreateTemplate.js
@@ -3,4 +3,4 @@ const { resolve } = require('path')
 
 const HexID = require('./HexID.js')
 
-writeFileSync(resolve(__dirname, '..', 'templates', `${HexID()}.hbs`), '\n')
+writeFileSync(resolve(__dirname, '..', 'templates', `${HexID()}.hbs`), '')
