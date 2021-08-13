7c2c5fabc666227b7a3e06de7faecf71d7f9c5c3
ruj <214782@bk.ru>
2021-08-13 13:54:33 -0300

    Template creation in JS and HBS

--- src/utils/CreateTemplate.js
+++ src/utils/CreateTemplate.js
@@ -3,4 +3,24 @@ const { resolve } = require('path')
 
 const HexID = require('./HexID.js')
 
-writeFileSync(resolve(__dirname, '..', 'templates', `${HexID()}.hbs`), '')
+const [, , extension] = process.argv
+
+if (!/hbs|js/.test(extension)) return
+
+const templatesPath = resolve(__dirname, '..', 'templates')
+const writeTemplate = (filename, data) =>
+  writeFileSync(resolve(templatesPath, extension, `${filename}.${extension}`), data)
+const filename = () => {
+  const id = HexID()
+  return /^\d/.test(id) ? filename() : id
+}
+
+Object.create({
+  hbs () {
+    writeTemplate(filename(), '')
+  },
+
+  js () {
+    writeTemplate(filename(), 'module.exports = (ctx) => {}\n')
+  }
+})[extension]()
