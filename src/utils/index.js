0027a53d402db2e9fa26756f6ef7d75c3fc78eb0
ruj <214782@bk.ru>
2021-08-20 17:19:43 -0300

    Text formatter

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,41 @@
 module.exports = class Utils {
   /**
+   * Text Formatting Options
+   * @typedef FormatTextOptions
+   * @type {Object}
+   * @property {boolean} [random] Randomize the informed formats
+   * @property {boolean} [unique] Only one format to apply
+   */
+
+  /**
+   * Add formatting to text
+   * @param {string} text Text to be formatted
+   * @param {string[]} format Formats to be applied to text
+   * @param {FormatTextOptions} options Options for text formatting
+   * @returns {string}
+   */
+  static formattedText (text, formats, options) {
+    const formatTags = (format) => {
+      if (format === 'none') return Array(2).fill('')
+
+      const firstLetter = format.match(/^[^\*]/).shift()
+
+      return [`[${firstLetter}]`, `[/${firstLetter}]`]
+    }
+
+    if (options?.random) formats = formats.shuffle()
+    if (options?.unique) formats = [formats.random()]
+
+    for (let index = 0; index < formats.length; index++) {
+      const [initialize, terminate] = formatTags(formats[index])
+
+      text = `${initialize}${text}${terminate}`
+    }
+
+    return text
+  }
+
+  /**
    * Slice a text into several lines
    * @param {string} text Text to be sliced
    * @param {number|number[]} numberOfLines Number of lines to define for new text
