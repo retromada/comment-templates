2dafb8a7e52c0007011421f21ba77e916bea45ef
ruj <214782@bk.ru>
2021-08-20 17:22:13 -0300

    1 files | M  src/templates/js/DF7A1724.js

--- src/templates/js/DF7A1724.js
+++ src/templates/js/DF7A1724.js
@@ -1,13 +1,27 @@
-const { slicingText } = require('../../utils')
+const { formattedText, slicingText } = require('../../utils')
 
 module.exports = (ctx) => {
+  const formats = ['bold', 'italic', 'underline', 'none']
+  const format = [formats.random()]
+
   return slicingText(ctx.text, [4, 6])
     .map((line, index, self) =>
       [
         ctx.unicode.emojis
-          .slice(index % 2 ? index * 1 + index : index * 2, index % 2 ? index * 2 + 2 : index * 2 + 2)
+          .slice(
+            index % 2 ? index * 1 + index : index * 2,
+            index % 2 ? index * 2 + 2 : index * 2 + 2
+          )
           .join(` ${ctx.unicode.symbol} `),
-        `${line}${index + 1 === self.length ? ` {${ctx.unicode.emojis.slice(-5).join('')}}` : ''}`
+        `${line.length ? formattedText(line, format) : line}${
+          index + 1 === self.length
+            ? ` ${formattedText(
+                `{${ctx.unicode.emojis.slice(-5).join('')}}`,
+                formats,
+                { unique: true }
+              )}`
+            : ''
+        }`,
       ].join(' ')
     )
     .join('\n')
