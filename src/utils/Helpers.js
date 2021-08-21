79a5ef6ee31581c77118d0940ac6688af8c7791f
ruj <214782@bk.ru>
2021-08-21 19:17:49 -0300

    Some other helpers array,formattedText,JSONparse,slicingText

--- src/utils/Helpers.js
+++ src/utils/Helpers.js
@@ -1,4 +1,10 @@
+const Utils = require('./')
+
 module.exports = [
+  ['array', (...values) => Array.prototype.slice.call(values, 0, -1)],
+  ['formattedText', Utils.formattedText],
   ['join', (array, separator) => array.join(separator)],
-  ['slice', (array, start, end) => array.slice(start, end)]
+  ['JSONparse', (string) => JSON.parse(string)],
+  ['slice', (array, start, end) => array.slice(start, end)],
+  ['slicingText', Utils.slicingText]
 ]
