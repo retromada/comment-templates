352ee803d35da33303681ab441c334987e387249
ruj <214782@bk.ru>
2021-08-21 19:25:38 -0300

    1 files | M  src/templates/js/F900F819.js

--- src/templates/js/F900F819.js
+++ src/templates/js/F900F819.js
@@ -6,7 +6,9 @@ module.exports = (ctx) => {
     ctx.text
       .split(' ')
       .map((word, index, self) =>
-        word !== self.slice(-1).pop() ? `${word} ${ctx.unicode.emojis[index]}` : word
+        word !== self.slice(-1).pop()
+          ? `${word} ${ctx.unicode.emojis[index]}`
+          : word
       )
       .join(' '),
     Object.values(TextFormats),
