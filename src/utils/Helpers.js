a721bbe927b34dcf4c2084d3747cbace4c3b76be
ruj <214782@bk.ru>
2021-08-22 09:18:11 -0300

    Template literals

--- src/utils/Helpers.js
+++ src/utils/Helpers.js
@@ -1,10 +1,11 @@
 const Utils = require('./')
 
 module.exports = [
-  ['array', (...values) => Array.prototype.slice.call(values, 0, -1)],
+  ['array', (...values) => values.slice(0, -1)],
   ['formattedText', Utils.formattedText],
   ['join', (array, separator) => array.join(separator)],
   ['JSONparse', (string) => JSON.parse(string)],
   ['slice', (array, start, end) => array.slice(start, end)],
-  ['slicingText', Utils.slicingText]
+  ['slicingText', Utils.slicingText],
+  ['templateLiterals', (...values) => values.slice(0, -1).join('')]
 ]
